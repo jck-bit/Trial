46bdb1372d48b9b7f17b9a1629b5e29a656c1a04
jck-bit <jckkinyanjui@gmail.com>
2022-05-30 10:02:59 +0300

    final

--- _site/Login/login.js
+++ _site/Login/login.js
@@ -1,3 +1,8 @@
 document.getElementById("submit").addEventListener("click", function() {
     window.location.href = "./Home page/Home.html";
   })
+
+
+document.getElementById("login").addEventListener("click", function() {
+   window.location.href = "./Admin/Login.html";
+});
